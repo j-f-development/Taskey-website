b3a0851c61b472e1919af8705d3682b48baff720
Fynn Schulz <finolino9@gmail.com>
2026-03-11 20:17:48 +0100

    dadadad

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
 import DemoBookingModal from "../DemoBookingModal";
 
@@ -12,19 +11,8 @@ export default function Hero() {
     <>
       <DemoBookingModal isOpen={demoModalOpen} onClose={() => setDemoModalOpen(false)} />
 
-      <section className="relative bg-white pt-24 pb-20 sm:pt-32 sm:pb-28 overflow-hidden">
-        {/* Background Image */}
-        <Image
-          src="/83CC6636-2805-434F-9DD7-E54950B72DF5.png"
-          alt="Taskey Hero Background"
-          fill
-          className="object-cover opacity-50"
-          priority
-        />
-        {/* Bottom fade */}
-        <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-white to-transparent z-[2]"></div>
-
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-left relative z-10">
+      <section className="bg-white pt-24 pb-20 sm:pt-32 sm:pb-28">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 text-left">
 
           {/* Headline */}
           <h1 className="text-7xl sm:text-8xl lg:text-9xl font-black text-gray-900 leading-[0.95] mb-8 tracking-tight">
